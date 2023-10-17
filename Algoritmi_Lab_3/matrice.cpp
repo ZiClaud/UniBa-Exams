41fd20893619510955f08c26a1a4a4944fcfc4ba
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-10-17 21:40:11 +0200

    Removed useless vscode files + Working on matrix
    Transpostato il progetto da vscode a CLion, rimossi file inutili

--- Algoritmi_Lab_3/matrice.cpp
+++ Algoritmi_Lab_3/matrice.cpp
@@ -6,16 +6,15 @@ Matrice::Matrice(int r, int c) {
 
     elementi = new tipoelem *[righe];
 
-    for (int i = 0; i < righe; i++)
-    {
+    for (int i = 0; i < righe; i++) {
         elementi[i] = new tipoelem[colonne];
     }
+
 }
 
 // Distruttore
 Matrice::~Matrice() {
-    for (int i = 0; i < righe; i++)
-    {
+    for (int i = 0; i < righe; i++) {
         delete[] elementi[i];
     }
     delete[] elementi;
@@ -25,5 +24,9 @@ tipoelem Matrice::leggimatrice(int r, int c) {
     return elementi[r][c];
 }
 
-Matrice A;
+void Matrice::scrivimatrice(int r, int c, tipoelem elem) {
+    elementi[r][c] = elem;
+}
+
+//Matrice A;
 // A.elementi[2][3];
--- Algoritmi_Lab_3/matrice.h
+++ Algoritmi_Lab_3/matrice.h
@@ -5,11 +5,13 @@ typedef double tipoelem;
 
 // template <class T>
 class Matrice {
-  public:
+public:
     Matrice(int, int); /* costruttore */
+    ~Matrice(); /* distruttore */
     tipoelem leggimatrice(int, int); // Al posto di scrivere "T" scrivo "tipoelem" -> In futuro sara' T
     void scrivimatrice(int, int, tipoelem);
-  private:
+
+private:
     int righe;
     int colonne;
     tipoelem **elementi;
