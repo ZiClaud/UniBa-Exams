46512743550dc0d99bda42fb9b9ba1ea0a288879
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-10-19 09:54:13 +0200

    lezione 19-10-23 finita

--- Lezioni/lezione_19-10-23.cpp
+++ Lezioni/lezione_19-10-23.cpp
@@ -1,5 +1,5 @@
 
-void f() {
+void f1() {
     int *p = new int;
     delete p;
 }
@@ -11,5 +11,25 @@ void f2() {
     p = new int; // Puntera' ad una nuova area
     delete p;
 }
+
+
+
+// Funzioni templatelezione_19-10-23
+template <typename T1, typename T2>
+T max(T1 x, T2 y);
+
+
+// Classi template
+template <class T>
+class A {
+    void f(T); // Generico
+    void f(int); // Specializzazione -> Se usi un int usa questo codice
+};
+
+
+
+
+
+
 
 
