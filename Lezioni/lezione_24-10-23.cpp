f53b0d7e5caca64bfa240c089e8ce44cb04c6ad7
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-10-24 09:42:16 +0200

    Working on lezione_24-10-23.cpp

---
+++ Lezioni/lezione_24-10-23.cpp
@@ -0,0 +1,57 @@
+
+// Passo base: Lista vuota
+A = [];
+
+// Lista ricorsiva:
+A = [X][L];  // Dove L e' una lista, X un elemento
+
+
+
+// Slide: Liste pg 10
+while (!finelista(p)) {
+    // ...
+    p = succlista();
+}
+
+// Slide: Liste pg. 12 - finelista
+listavuota = finelista(primolista());   // Se la lista e' vuota -> E' falso
+
+// Slide: Liste pg. 13 - inslista
+[a1, a2, a3] ->
+pos(2) [a1, a2, a, a3] o [a1, a, a2, a3]?
+
+[a1, a, a2, a3]
+
+
+// Slide: Liste pg. 16 - Pseudocodice
+duplicati(L)    // Posso applicarla solo se la lista non e' vuota
+    if (!listavuota(L))
+        p = primolista(L)
+        while (!finelista(p, L))
+            q = succlisa(p, L)
+            while (!finelista(q, L)
+                if (leggilista(p, L) == leggilista(q, L)
+                    canclista(q, L)
+                q = succlista(q, L)
+            p = succlista(p, L)
+
+
+// Ora in C++
+template <class T> // Funziona solo quando T e' un tipo primitivo (== funziona solo per i primitivi)
+void duplicati(Lista<T> L) {
+    Listaz<T>::posizione p,q;
+    if (!L.listavuota()) {
+        p = L.primolista();
+        while (!L.finelista(p)) {
+            q = L.succlisa(p)
+            while (!L.finelista(q) {
+                if (L.leggilista(p) == L.leggilista(q) {
+                    L.canclista(q);
+                }
+                q = L.succlista(q);
+            }
+            p = L.succlista(p);
+        }
+    }
+}
+
