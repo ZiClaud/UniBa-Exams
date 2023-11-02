47cda0e2ea391566c9e89d2401c15e4de91b3087
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2023-11-02 12:28:27 +0100

    Worked on list_operations

---
+++ Algoritmi_Lab_5/list_operations.cpp
@@ -0,0 +1,23 @@
+#include "list_operations.h"
+
+template<class T>
+int List_Operations<T>::lunghezza(list<T> l) {
+    return l.size();
+}
+
+template<class T>
+void List_Operations<T>::inverti(list<T> &l) {
+    l.reverse();
+}
+
+template<class T>
+bool List_Operations<T>::palindoma(list<T> l) {
+    T item;
+    for (int i = 0; i < lunghezza(l); ++i) {
+        if (l[i] != l[lunghezza(l) - i]){
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ Algoritmi_Lab_5/list_operations.h
@@ -0,0 +1,16 @@
+#ifndef ALGORITMI_LAB_LIST_OPERATIONS_H
+#define ALGORITMI_LAB_LIST_OPERATIONS_H
+
+#include <list>
+using namespace std;
+
+template<class T>
+class List_Operations {
+public:
+    int lunghezza(list<T>);
+    void inverti(list<T> &);
+    bool palindoma(list<T>);
+};
+
+
+#endif //ALGORITMI_LAB_LIST_OPERATIONS_H
