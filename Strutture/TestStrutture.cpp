72be842b2ba9ce31f3f83c26a1d3aa74359ee355
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2024-01-05 19:10:07 +0100

    Working on MyListVector

--- Strutture/TestStrutture.cpp
+++ Strutture/TestStrutture.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
-#include "MyList/MyList.h"
+#include "MyListVector/MyListVector.h"
 
 using namespace std;
 
 int main() {
+    ///Test MyListVector
     cout << "Hello" << endl;
-    MyList<int> l;
+    MyListVector<int> l;
     l.print();
     cout << "IsEnd(2):" << l.isEnd(2) << endl;
     cout << "IsEmpty:" << l.isEmpty() << endl;
+    // TODO: Test without "add" first
+    l.add(4);
+    l.add(3);
+    l.add(2);
+    l.add(1);
+    l.print();
     l.write(1, 0);
     l.write(2, 1);
     l.write(3, 2);
@@ -27,6 +34,8 @@ int main() {
     cout << "IsEnd(2):" << l.isEnd(2) << endl;
     cout << "IsEnd(24):" << l.isEnd(24) << endl;
     cout << "IsEmpty:" << l.isEmpty() << endl;
+
+    ///Test MyStack (Pila)
 
     return 0;
 }
