77d2ccf2519c36d6947435e6f9750f44603ee3c8
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2024-04-03 10:57:30 +0200

    Improved Python part of exam 20-11-14

--- exams/e-20-11-14.py
+++ exams/e-20-11-14.py
@@ -8,13 +8,32 @@ from typing import List
 
 
 def mediana_frazione(vec_x: List[int]):
+    if len(vec_x) == 0:
+        raise ValueError("No number found")
+    elif len(vec_x) == 1:
+        return vec_x[0]
+    if (vec_x[0] <= 0):
+        raise ValueError("Negative number found")
     return vec_x[0] + 1 / _mediana_frazione(vec_x, 1)
 
 
 def _mediana_frazione(vec_x: List[int], curr: int):
+    if (vec_x[curr] <= 0):
+        raise ValueError("Negative number found")
     if curr + 1 == len(vec_x):
         return vec_x[curr]
     return vec_x[curr] + 1 / _mediana_frazione(vec_x, curr + 1)
 
 
+print(mediana_frazione([3]))
 print(mediana_frazione([3, 4, 12, 4]))
+
+try:
+    print(mediana_frazione([]))
+except ValueError:
+    print("Value Error")
+
+try:
+    print(mediana_frazione([3, 4, -12, 4]))
+except ValueError:
+    print("Value Error")
