639bc66c97e73dfaaa10b5d24850a8cb65fd4221
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2024-04-06 17:18:33 +0200

    Done Python part 2 of exam 04-04-13

--- exams/e-04-04-13.py
+++ exams/e-04-04-13.py
@@ -6,6 +6,7 @@ Esercizio. Scrivere una funzione Python che abbia:
 - OUTPUT: Un intero n che rappresenta il numero minimo di termini da considerare nella formula
 per raggiungere un’approssimazione di pi con errore assoluto non superiore a tol
 """
+from numpy import sqrt
 
 
 def leibniz(tol: float, itmax: int) -> int:
@@ -31,10 +32,10 @@ def _leibniz(tol: float, itmax: int, itcurr: int, val: float, currmult: int) -> 
     return _leibniz(tol, itmax, itcurr + 1, currval, currmult + 2)
 
 
-print(leibniz(1, 500))
-print(leibniz(0.1, 500))
-print(leibniz(0.01, 500))
-print(leibniz(0.001, 500))
+# print(leibniz(1, 500))
+# print(leibniz(0.1, 500))
+# print(leibniz(0.01, 500))
+# print(leibniz(0.001, 500))
 
 """
 Ripetere l’esercizio considerando, in luogo della (2), la seguente formula (di Viete):
@@ -43,4 +44,64 @@ In tal caso il parametro di output n rappresenta il numero minimo di fattori da 
 seconda formula per ottenere un’approssimazione di ⇡ con errore assoluto minore di tol.
 """
 
-# TODO
+
+def _vietetest(it: int) -> float:
+    val: float = 2 * (2 / sqrt(2))
+    itcurr: int = 1
+
+    while itcurr < it:
+        i: int = 0
+        den: float = 2
+
+        while i < itcurr:
+            den = 2 + sqrt(den)
+            i += 1
+
+        val = val * (2 / sqrt(den))
+        itcurr += 1
+        # print(val)
+
+    # print(2.0)
+    # print(2 * 2 / sqrt(2))
+    # print(2 * 2 / sqrt(2) * 2 / sqrt(2 + sqrt(2)))
+    # print(2 * 2 / sqrt(2) * 2 / sqrt(2 + sqrt(2)) * 2 / sqrt(2 + sqrt(2 + sqrt(2))))
+    return val
+
+
+#_vietetest(100)
+
+
+def viete(tol: float, itmax: int) -> int:
+    val: float = 2 * (2 / sqrt(2))
+    oldval: float
+    itcurr: int = 1
+
+    while itcurr < itmax:
+        oldval = val
+        i: int = 0
+        den: float = 2
+
+        while i < itcurr:
+            den = 2 + sqrt(den)
+            i += 1
+
+        val = val * (2 / sqrt(den))
+
+        if abs(oldval - val) < tol:
+            print("Tolleranza raggiunta")
+            print("Pi: " + str(val))
+            return itcurr
+
+        itcurr += 1
+
+    print("Tolleranza non raggiunta")
+    print("Pi: " + str(val))
+    return itcurr
+
+
+print(viete(1, 500))
+print(viete(0.1, 500))
+print(viete(0.01, 500))
+print(viete(0.001, 500))
+print(viete(0.0001, 500))
+print(viete(0.000000000001, 500))
