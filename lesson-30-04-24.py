bb32aee89b72c4fdc11d06b109d4d9146a108e92
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2024-05-02 19:44:03 +0200

    Working on lesson-30-04-24.py

--- lesson-30-04-24.py
+++ lesson-30-04-24.py
@@ -7,8 +7,41 @@ Scrivere una funzione Python che ha in input una matrice A generica e restituisc
 
 
 def riduci_a_scalini(A: np.array) -> np.array:
-    # TODO
-    pass
+    for j in range(len(A)):
+        A = sottrai(A, j)
+        # A[j][i] = i
+
+    return A
+
+
+def sottrai(A: np.array, j_curr: int) -> np.array:
+    curr_row: np.array = A[j_curr]
+    j: int = 0
+
+    for row in A:
+        i: int = 0
+        den: float = 0
+        for val in row:
+            if j_curr >= j:
+                break
+            if curr_row[i] == 0:  # or np.array_equal(curr_row, row)
+                i += 1
+                continue
+            elif den == 0:
+                den = - val / curr_row[i]
+                print("Den: ", den)
+                print("-", val, "/", curr_row[i])
+
+            if den != 0:
+                print("val = ", val, "+", curr_row[i], "*", den)
+                val = val + curr_row[i] * den
+                print(val)
+
+            row[i] = val
+            i += 1
+        j += 1
+
+    return A
 
 
 """
@@ -24,7 +57,11 @@ def get_rango(A: np.array) -> int:
 
 
 if __name__ == '__main__':
-    A = np.array([[1, 2, 3], [4, 5, 6]])
+    A = np.array([[1, 2, 3], [1, 3, 4]])
+
+    # print(sottrai(np.array([[1, 2], [3, 4]]), 0))
+    # print(sottrai(np.array([[1, 2], [3, 4]]), 1))
+
     print(A)
     print(riduci_a_scalini(A))
     print(get_rango(A))
