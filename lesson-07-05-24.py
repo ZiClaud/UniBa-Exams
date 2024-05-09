5ce9113eecaa156111dd0c0d9abd825264237d91
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2024-05-09 17:57:52 +0200

    Done lesson-07-05-24.py ex 1

--- lesson-07-05-24.py
+++ lesson-07-05-24.py
@@ -4,7 +4,18 @@ import numpy as np
 1) Scrivere una funzione python che abbia in input un vettore x e restituisca in output la matrice V di Vandermonde
 """
 
-# TODO
+
+def vandermonde_matrix(x: np.array) -> np.array:
+    n: int = len(x)
+
+    V: np.array = np.zeros((n, n))
+
+    for i in range(n):
+        for j in range(n):
+            V[i][j] = pow(x[i], j)
+
+    return V
+
 
 """
 2) Scrivere una funzione python che abbia un input due valori x e y delle stesse lunghezze e resituisca, in output, 
@@ -15,9 +26,6 @@ i coefficienti del polinomio interpolante in un vettore a
 
 
 if __name__ == '__main__':
-    A = np.array([[1, 2, 3], [1, 3, 4]])
-    A = np.array([[1, 2, 3], [1, 3, 4], [4, 3, 5]])
+    x: np.array = np.array([1, 2, 3])
 
-    A = np.array([[2., -1, 1], [4, -2, 2], [1, 3, 11], [1, -4, -10]])
-
-    print(A)
+    print(vandermonde_matrix(x))
